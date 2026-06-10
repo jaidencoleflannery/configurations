6fc1fa7fcd566ceb212426ced676dc68d2b019a1
Cole Flannery <jaidencoleflannery@gmail.com>
2026-06-09 23:57:13 -0500

    nvim | finished mockup status line plugin.

--- nvim-test-env/plugin/statusline.lua
+++ nvim-test-env/plugin/statusline.lua
@@ -1,27 +1,26 @@
-local output_padding = " "
+local attached_buffers = {}
+local current_buffer = vim.api.nvim_get_current_buf()
 
-local function sync_output(ev)
-	vim.api.nvim_buf_attach(0, false, {
-		on_lines = function(_, bufnr, changedtick, firstline, lastline, new_lastline)
-		end,
-	})
-
+local function sync_output(ev)		
 	local available_character_count = vim.api.nvim_win_get_width(0)
 	local character_count = 0
 
 	local output_prefix_buffer = ""
 	local output_suffix_buffer = ""
 	local output_buffer = ""
+	local output_padding = " "
 
 	local current_file = vim.bo.filetype
 	local current_file_output = ""
 	local has_file_open = true
 
+	local relative_path = ""
+
 	if current_file ~= "" then
 		current_file_output = current_file
 		has_file_open = true
 	else
-		current_file_output = "home"
+		current_file_output = "nvim"
 		has_file_open = false
 	end
 
@@ -30,23 +29,29 @@ local function sync_output(ev)
 	-- filetype.
 	output_prefix_buffer = output_padding .. "[" .. current_file_output .. "]" .. output_padding
 	
-	if has_file_open == true and tostring(vim.bo.modified) == true then
-		output_prefix_buffer = output_prefix_buffer .. "[+]" -- has changes.
+	if has_file_open == true and vim.bo.modified then
+		output_prefix_buffer = output_prefix_buffer .. "[+]" .. output_padding -- has changes.
 	elseif vim.bo.readonly == true then
-		output_prefix_buffer = output_prefix_buffer .. "[-]" -- readonly.
+		output_prefix_buffer = output_prefix_buffer .. "[-]" .. output_padding -- readonly.
 	end
+
+	-- filepath.
+	relative_path = vim.fn.expand('%')
+	if relative_path == "." then
+		relative_path = "./"
+	end
+	output_prefix_buffer = output_prefix_buffer .. relative_path .. output_padding
 
 	available_character_count = available_character_count - #output_prefix_buffer
 
 	--suffix display.
 
 	-- get user opened buffers.
-	local buffer_quantity = vim.fn.getbufinfo({ buflisted = 1 })
-	local current_buffer = vim.api.nvim_get_current_buf()
+	local buffer_quantity = vim.fn.getbufinfo({ buflisted = 1 })	
 	if current_buffer == nil then
-		current_buffer = 999
+		current_buffer = 0
 	end
-	output_suffix_buffer = output_suffix_buffer .. "[" .. tostring(current_buffer) .. ":" .. tostring(#buffer_quantity) .. "]" .. output_padding
+	output_suffix_buffer = output_suffix_buffer .. tostring(current_buffer) .. ":" .. tostring(#buffer_quantity) .. output_padding
 
 	available_character_count = available_character_count - #output_suffix_buffer
 
@@ -66,7 +71,20 @@ local function sync_output(ev)
 end
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-	callback = sync_output
+	callback = function()
+		current_buffer = vim.api.nvim_get_current_buf()
+		if not attached_buffers[current_buffer] then
+			attached_buffers[current_buffer] = true
+			vim.api.nvim_buf_attach(current_buffer, false, {
+				on_lines = function()
+					sync_output()
+				end,
+				on_detach = function(_, detached_buffer)
+					attached_buffers(detached_buffer) = nil
+				end
+			})
+		end
+		sync_output()
 	end,
 })
 
